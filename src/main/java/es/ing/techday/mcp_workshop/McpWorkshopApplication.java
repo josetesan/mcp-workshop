d000966cb83069574082e7d89423a01bec49b94c
jose luis sanchez romero <muallin@gmail.com>
2025-05-27 21:31:17 +0200

    Initial status

---
+++ src/main/java/es/ing/techday/mcp_workshop/McpWorkshopApplication.java
@@ -0,0 +1,13 @@
+package es.ing.techday.mcp_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ing/techday/mcp_workshop/McpWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ing.techday.mcp_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
